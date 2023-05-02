cd9c48c123f4557c16003c31131141a91795b7cb
webdev <117325718+webdevcodeman@users.noreply.github.com>
2023-05-02 19:36:13 +0530

    Learn JS

--- alert.js
+++ alert.js
@@ -1,1 +1,14 @@
-alert("I’m JavaScript!");
+"use strict";
+// alert("I’m JavaScript!");
+
+// let admin;
+// let name;
+// name = "John";
+// admin = name;
+// alert( admin );
+
+// let ourPlanetName = "Earth";
+// let currentUserName = "John";
+
+let name = prompt("Enter your name:", "");
+alert(name);
